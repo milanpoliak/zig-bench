ce489013d6c5c7492b3e343f86496be9b606046c
Milan Poliak <milan.poliak@wize.sk>
2024-07-02 01:36:21 +0200

    simplify build file

--- build.zig
+++ build.zig
@@ -16,9 +16,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("zig-bench", .{
-        .root_source_file = .{
-            .src_path = .{ .sub_path = "src/zig-bench.zig", .owner = b }
-        },
+        .root_source_file = b.path("src/zig-bench.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -26,9 +24,7 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_source_file = .{
-            .src_path = .{ .sub_path = "src/zig-bench.zig", .owner = b }
-        },
+        .root_source_file = b.path("src/zig-bench.zig"),
         .target = target,
         .optimize = optimize,
     });
